433014bf1ac69dd961317c003ea5ca76b673d104
James Laugerman <jameslaugerman@gmail.com>
2021-02-15 08:25:58 -0600

    espresso menu added

--- src/shared/menudata.js
+++ src/shared/menudata.js
@@ -14,19 +14,85 @@ export const MENUDATA = [
       item: "Pour-Over",
       description: "Rotating Daily Selection",
        price: {
-       small: 3.5,
-       large: 4 
+       small: 4
+      },
+      menu: "cafe",
+      submenu: "drip coffee",
+    },
+    { 
+      id: 3,
+      item: "French Roast",
+      description: "dark blend - dark chocolate, smokey, full-bodied",
+      price: {
+       small: 3
+      },
+      menu: "cafe",
+      submenu: "drip coffee",
+    },
+    { 
+      id: 4,
+      item: "Decaf",
+      description: "Swiss Water Process - molasses, cocoa, smooth",
+      price: {
+       small: 3,
       },
       menu: "cafe",
       submenu: "drip coffee",
     },
       { 
-      id: 3,
+      id: 5,
       item: "Espresso (single or double)",
-      description: "Rotating Daily Selection",
+      description: "",
+      price: {
+       small: 3,
+       large: 4 
+      },
+      menu: "cafe",
+      submenu: "espresso",
+    },
+    { 
+      id: 6,
+      item: "Americano",
+      description: "",
+      price: {
+       small: 3,
+       large: 4 
+      },
+      menu: "cafe",
+      submenu: "espresso",
+    },
+    { 
+      id: 7,
+      item: "Cappucino",
+      description: "",
       price: {
        small: 3.5,
-       large: 4 
+       medium: 4,
+       large: 4.5 
+      },
+      menu: "cafe",
+      submenu: "espresso",
+    },
+    { 
+      id: 8,
+      item: "Cafe Latte",
+      description: "",
+      price: {
+       small: 3.5,
+       medium: 4,
+       large: 4.5 
+      },
+      menu: "cafe",
+      submenu: "espresso",
+    },
+    { 
+      id: 9,
+      item: "Cafe Mocha",
+      description: "",
+      price: {
+       small: 4,
+       medium: 4.5,
+       large: 5 
       },
       menu: "cafe",
       submenu: "espresso",
